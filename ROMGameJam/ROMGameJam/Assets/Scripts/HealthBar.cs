4b1081a35475b0bf972af7182357f204b0557422
g5chohon <hongman.cho@gmail.com>
2016-09-18 02:58:07 -0400

    added romgamejam

---
+++ ROMGameJam/ROMGameJam/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/// <summary>
+/// WORLD SPACE CANVAS FACES PLAYER AT ALL TIMES
+/// </summary>
+public class HealthBar : MonoBehaviour
+{
+    private Transform progressCanvas;
+    private Transform progressBar;
+    private Image progressBarImg;
+    private float timer;
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    private Transform playerStatsCanvas;
+    public float exp;
+    public float dmg;
+    public float health;
+
+
+    void Start()
+    {
+        playerStatsCanvas = GameObject.Find("PlayerStatsCanvas").transform;
+        progressCanvas = transform.Find("ProgressCanvas");
+        /*
+        if (!progressCanvas.gameObject.activeSelf) {
+            progressCanvas.gameObject.SetActive(true);
+        }
+        */
+        progressBar = progressCanvas.Find("ProgressBG");
+        progressBarImg = progressBar.Find("Progress").GetComponent<Image>();
+        progressCanvas.gameObject.SetActive(false);
+        timer = 0;
+    }
+
+    void Update()
+    {
+        // if canvas active, keep it 0 degrees.
+        if (progressCanvas.gameObject.activeSelf) {
+            progressCanvas.rotation = Quaternion.Euler(0, 0, 0);
+            timer += Time.deltaTime;
+        }
+        // turn off canvas after 3 sec display
+        if (timer >= 3) {
+            progressCanvas.gameObject.SetActive(false);
+            timer = 0;
+        }
+    }
+
+    public void damageDisplay(float damage) {
+        timer = 0;
+        if (!progressCanvas.gameObject.activeSelf) {
+            progressCanvas.gameObject.SetActive(true);
+        }
+        // if this.mass <= player.mass, do nothing (just bump into each other)
+        // if otherwise, dmg the player based on mass difference.
+        progressBarImg.fillAmount -= damage;
+        //Debug.Log(progressBarImg.fillAmount);
+        // if player health <= 0, destroy the player.
+        if (progressBarImg.fillAmount <= 0) {
+            // explode this creature
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+            playerStatsCanvas.GetComponent<PlayerStats>().rewardExp(exp);
+        }
+    }
+}
---
+++ ROMGameJam/ROMGameJam/Assets/Scripts/Plants.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plants : MonoBehaviour {
+
+    public float scrollSpeed;
+    private Vector2 savedOffset;
+    private Vector3 velocity;
+    public GameObject player;
+    private Vector3 previousVelocity;
+    private float x, y;
+    private float x_plustimer, x_minustimer, y_plustimer, y_minustimer;
+
+    void Start() {
+        savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset("_MainTex");
+        velocity = player.GetComponent<Rigidbody2D>().velocity;
+        previousVelocity = velocity;
+        x = transform.position.x;
+        y = transform.position.y;
+        x_plustimer = 0;
+        x_minustimer = 0;
+        y_plustimer = 0;
+        y_minustimer = 0;
+    }
+
+    void FixedUpdate() {
+        previousVelocity = velocity;
+    }
+
+    void Update() {
+        velocity = player.GetComponent<Rigidbody2D>().velocity;
+
+        if (velocity.sqrMagnitude >= previousVelocity.sqrMagnitude && velocity.sqrMagnitude != 0) {
+            //Debug.Log("vel: " + velocity.sqrMagnitude);
+            //Debug.Log("prev vel: " + previousVelocity.sqrMagnitude);
+            if (velocity.x > 0) {
+                //x_plustimer += Time.deltaTime;
+                //x = Mathf.Repeat(scrollSpeed * x_plustimer, 1);
+                x -= scrollSpeed * velocity.x / 10 * Time.deltaTime;
+            } else if (velocity.x < 0) {
+                //x_plustimer += Time.deltaTime;
+                //x = Mathf.Repeat(-scrollSpeed * x_plustimer, 1);
+                x -= scrollSpeed * velocity.x / 10 * Time.deltaTime;
+            }
+
+            if (velocity.y > 0) {
+                //y_plustimer += Time.deltaTime;
+                //y = Mathf.Repeat(scrollSpeed * y_plustimer, 1);
+                y -= scrollSpeed * velocity.y / 10 * Time.deltaTime;
+            } else if (velocity.y < 0) {
+                //y_plustimer += Time.deltaTime;
+                //y = Mathf.Repeat(scrollSpeed * y_plustimer, 1);
+                y -= scrollSpeed * velocity.y / 10 * Time.deltaTime;
+            }
+            //float x = Mathf.Repeat(velocity.x % 1 * scrollSpeed * Time.time, 1);
+            //Debug.Log("x: " + x);
+            //float y = Mathf.Repeat(velocity.y % 1 * scrollSpeed * Time.time, 1);
+            //Debug.Log("y: " + y);
+            //Debug.Log(velocity);
+            //Vector2 offset = new Vector2(x, y);
+            //GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+            transform.position = new Vector2(x, y);
+        }
+        //previousVelocity = velocity;
+    }
+
+    void OnDisable() {
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", savedOffset);
+    }
+}
