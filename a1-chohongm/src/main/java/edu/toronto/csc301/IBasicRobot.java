9ccf1fe32cdb966fade0badf0a00009ac00415f9
g5chohon <hongman.cho@gmail.com>
2016-09-20 22:04:43 -0400

    added java files.

--- a1-chohongm/src/main/java/edu/toronto/csc301/IBasicRobot.java
+++ a1-chohongm/src/main/java/edu/toronto/csc301/IBasicRobot.java
@@ -5,8 +5,10 @@ public interface IBasicRobot {
 	public double getXCoordinate();
 	public double getYCoordinate();
 	public int getRotation();
-
+	
 	public void rotateRight(int degrees);
 	public void rotateLeft(int degrees);
 	public void moveForward(int millimeters);
+	
+	
 }
