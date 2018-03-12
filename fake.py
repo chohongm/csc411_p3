f8d4135fd58e04379913775941b1dc51bbd7d73d
Shawnee Rizqa <shawnee.rizqa@mail.utoronto.ca>
2018-03-12 18:24:18 -0400

    part1

--- fake.py
+++ fake.py
@@ -17,47 +17,6 @@ def get_words_count(data):
 
     return words_count
 
-
-def split_data(dataset1, dataset2):
-
-    training, validation, test = [], [], []
-
-    # number of headlines in each dataset
-    count1 = len(open(dataset1).readlines())
-    count2 = len(open(dataset2).readlines())
-
-    training_count1 = int(count1 * 0.7)
-    training_count2 = int(count2 * 0.7)
-    val_count1 = int(count1 * 0.15)
-    val_count2 = int(count2 * 0.15)
-
-    # add first dataset to list
-    cur_count1 = 0
-    for line in open(dataset1):
-        words = line.split()
-        if cur_count1 < training_count1:
-            training.append(words)
-        elif cur_count1 < (training_count1 + val_count1):
-            validation.append(words)
-        elif cur_count1 < count1:
-            test.append(words)
-        cur_count1 += 1
-
-    # add second dataset to list
-    cur_count2 = 0
-    for line in open(dataset2):
-        words = line.split()
-        if cur_count2 < training_count2:
-            training.append(words)
-        elif cur_count2 < (training_count2 + val_count2):
-            validation.append(words)
-        elif cur_count2 < count2:
-            test.append(words)
-        cur_count2 += 1
-
-    return training, validation, test
-
-
 def part1():
 
     # get words count from each real and fake dataset
@@ -91,20 +50,12 @@ def part1():
     print "# of appearances in real headlines: ", real_words['the']
     print "# of appearances in fake headlines: ", fake_words['the']
 
-    # split dataset
-    training, validation, test = split_data("clean_real.txt", "clean_fake.txt")
 
-    return training, validation, test
 
 if __name__ == '__main__':
 
     ##### PART 1 ######
-    training, validation, test = part1()
-
-    # print training
-    # print validation
-    # print test
-
+    part1()
 
 
 
